46326eba837c9727aae50ed96cae4625bf53499c
Ubaid Raza <154611273+ubaiidraza@users.noreply.github.com>
2025-01-06 17:38:31 +0500

    completed

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -11,5 +11,24 @@ router.get("/products/:id", getSingleProduct);
 router.put("/updateProduct/:id", upload.single("image"), updateProduct);
 router.delete("/deleteProduct/:id", deleteProduct);
 
+      
+export default router;  
 
-export default router;
+
+
+
+// import express from "express";
+// import { addProduct, deleteProduct, getProducts, getSingleProduct, updateProduct } from "../controllers/product.controllers.js";
+// import { upload } from "../middleware/multer.middleware.js"
+
+
+// const router = express.Router();
+
+// router.post("/addProducts", upload.single("image"), addProduct);
+// router.get("/products", getProducts);
+// router.get("/products/:id", getSingleProduct);
+// router.put("/updateProduct/:id", upload.single("image"), updateProduct);
+// router.delete("/deleteProduct/:id", deleteProduct);
+
+
+// export default router;
